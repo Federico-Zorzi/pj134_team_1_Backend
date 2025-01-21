6ae4295bdc4311bcf7f346d635c507e115602779
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-01-21 16:36:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// * DOTENV(.env) SETUP FOR GENERAL CONFIGURATION
+require("dotenv").config();
+
+// * EXPRESS SETUP
+const express = require("express");
+const app = express();
+const https = process.env.HTTPS === "on" ? "https" : "http";
+const host =
+  process.env.HOST + (process.env.PORT ? ":" + process.env.PORT : "");
+const port = process.env.PORT;
+
+// * MIDDLEWARES REGISTERING
+const notFound = require("./middlewares/notFound");
+const errorHandler = require("./middlewares/errorHandler");
+
+//  * DATA PARSING FOR METHODS POST/PUT/PATCH
+app.use(express.json());
+
+// * REGISTERING PUBLIC FOLDER
+app.use(express.static("public"));
+
+// * ROUTERS SETUP
+const homepageRouter = require("./routers/homepage");
+app.use("/", homepageRouter);
+
+// * MIDDLEWARES FOR ERRORS
+app.use(errorHandler);
+app.use(notFound);
+
+// * SERVER LISTEN
+app.listen(3000, () => {
+  console.log(`Server listening at ${https}://${host}`);
+});
