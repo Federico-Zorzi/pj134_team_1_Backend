4b607f35dca542d96aa13fd35842a1e3d1b6262a
MattiaPlacido <placido.mattia@gmail.com>
2025-01-23 18:10:32 +0100

    store user funzionante con controlli

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -29,4 +29,45 @@ function show(req, res) {
   });
 }
 
-module.exports = { index, show };
+//store
+function store(req, res) {
+  const { email, name, surname, telephone } = req.body;
+
+  console.log(email, name, surname, telephone);
+
+  //controllo parametri essenziali
+  if (!name || !surname || !email) {
+    return res.status(400).json({ error: "Mancano parametri essenziali" });
+  }
+
+  //controllo che il numero di telefono sia un numero
+  if (telephone && isNaN(telephone)) {
+    return res
+      .status(400)
+      .json({ error: "il numero di telefono deve essere un numero" });
+  }
+
+  //controllo se la mail esiste già
+  const checkEmailSql = "SELECT * FROM users WHERE email = ?";
+  connection.query(checkEmailSql, [email], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database error" });
+
+    if (results.length > 0) {
+      return res.status(400).json({ error: "L'email è già registrata" });
+    }
+    //controllo di validità della mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email && !emailRegex.test(email)) {
+      return res.status(400).json({ error: "L'email inserita è invalida" });
+    }
+
+    const sql =
+      "INSERT INTO users(email, name, surname, telephone) VAlUES (?,?,?,?)";
+    connection.query(sql, [email, name, surname, telephone], (err, results) => {
+      if (err) return res.status(500).json({ error: "Database error" });
+      res.status(201).json({ message: "Utente aggiunto con successo!" });
+    });
+  });
+}
+
+module.exports = { index, show, store };
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,6 +3,9 @@ const express = require("express");
 //Permessi di routing da express
 const usersRouter = express.Router();
 
+//connessione al db
+const connection = require("../db_connection");
+
 //import del controller
 const usersController = require("../controllers/usersController");
 
@@ -12,5 +15,11 @@ usersRouter.get("/", usersController.index);
 
 //show
 usersRouter.get("/:id", usersController.show);
+
+//email index
+usersRouter.get("/emails");
+
+//store
+usersRouter.post("/add", usersController.store);
 
 module.exports = usersRouter;
