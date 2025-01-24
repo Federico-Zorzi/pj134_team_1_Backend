a2e7413969da4d9901862026b9b1961269eff4e3
MattiaPlacido <placido.mattia@gmail.com>
2025-01-24 11:40:38 +0100

    get properties by user id

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,8 +1,8 @@
 //* INDEX USERS : host/users/
 //* SHOW USER : host/users/specificuser
+//* SHOW PROPERY BY USER ID :
 //* INDEX USERS EMAILS : host/users/emails
-//* STORE USER : users/add
-
+//* STORE USER : host/users/getproperties/:userid
 //Inizializzazione express
 const express = require("express");
 //Permessi di routing da express
@@ -32,6 +32,25 @@ usersRouter.get("/emails", (req, res) => {
 
 //show
 usersRouter.get("/specificuser", usersController.show);
+
+//show properties by id
+usersRouter.get("/getproperties/:id", (req, res) => {
+  const { id } = req.params;
+  const sql =
+    "SELECT properties.* FROM properties JOIN users ON properties.owner_id = users.id WHERE users.id = ?";
+
+  if (isNaN(id) || id < 1) {
+    return res.status(400).json({ error: "Id inserito non valido" });
+  }
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (results.length < 1) {
+      return res.status(404).json({ error: "Id non trovato" });
+    }
+    res.json(results);
+  });
+});
 
 //store
 usersRouter.post("/add", usersController.store);
